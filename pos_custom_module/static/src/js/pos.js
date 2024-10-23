5dcd3805684b1c692c11ed6bb77b308dc7db58e6
ahmed <ahmed.abdelbakey.18@gmail.com>
2024-10-23 16:30:12 +0300

    apply rpc on js file

--- pos_custom_module/static/src/js/pos.js
+++ pos_custom_module/static/src/js/pos.js
@@ -13,6 +13,16 @@ odoo.define('pos_custom_module.pos', function(require){
 
         }
         async CustomBtnClick(){
+        var result = await this.rpc({
+                'model': 'res.lang',
+                'method': 'search_read',
+                'args':[[],['id', 'name', 'code']]
+        })
+        result.forEach(function(values){
+                console.log("kkkkkkkkkkk", values)
+
+        })
+        console.log("kkkkkkkkkkk", result)
 //                this.showPopup('ErrorPopup',{
 //                title: 'Error',
 //                body: 'You are Not allow to use this button',
@@ -32,11 +42,11 @@ odoo.define('pos_custom_module.pos', function(require){
 //                title: this.env._t('Offline Error'),
 //                body: this.env._t('Test'),
 //                })
-                const {confirmed, payload:selectedOption } = await this.showPopup('SelectionPopup', {
-                title: 'Are You JS Developer',
-                list: [{'id':0, 'label': this.env._t('Yes'), 'item':'Yes'},
-                {'id':1, 'label': this.env._t('No'), 'item':'No'}]
-                });
+//                const {confirmed, payload:selectedOption } = await this.showPopup('SelectionPopup', {
+//                title: 'Are You JS Developer',
+//                list: [{'id':0, 'label': this.env._t('Yes'), 'item':'Yes'},
+//                {'id':1, 'label': this.env._t('No'), 'item':'No'}]
+//                });
                 console.log("You are in Custom Button")
         }
 
